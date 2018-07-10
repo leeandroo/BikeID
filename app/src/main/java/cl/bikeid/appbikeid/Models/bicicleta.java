cadba7f34318ec093defed51b7b089de18189f0f
leeandroo <leandro.cisterna02@inacapmail.cl>
2018-07-10 17:21:05 -0400

    Prueba de commit

---
+++ app/src/main/java/cl/bikeid/appbikeid/Models/bicicleta.java
@@ -0,0 +1,13 @@
+package cl.bikeid.appbikeid.Models;
+
+public class bicicleta {
+    private int codBicicleta;
+    private String marca;
+    private String color;
+    private String aro;
+    private char img;
+
+    //Constructores
+    public bicicleta(){ }
+
+}
